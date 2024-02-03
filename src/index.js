ba84e4fcb2ab2587d69d9246a68d671a98716656
Nick <rinaldinick88@gmail.com>
2024-02-03 17:23:31 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const https = require('https');
+const today = new Date();
+const yesterday = new Date(today);
 
-https.get('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard', (res) => {
+yesterday.setDate(yesterday.getDate() - 1);
+const yyyyMMdd = yesterday.toISOString().split('T')[0].replace(/-/g, '');
+
+console.log(yyyyMMdd);
+
+const url = `https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard?dates=${yyyyMMdd}`;
+
+https.get(url, (res) => {
     let data = '';
 
     // A chunk of data has been received.
     res.on('data', (chunk) => {
         data += chunk;
+        // console.log(data);
     });
+
+
 
     // The whole response has been received. Process the result.
     res.on('end', () => {
@@ -14,10 +26,13 @@ https.get('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboa
         if (jsonData.events) {
             jsonData.events.forEach(event => {
                 if (event.competitions && event.competitions[0].odds && event.competitions[0].odds[0].overUnder) {
+                    // console.log("true");
                     const overUnder = event.competitions[0].odds[0].overUnder;
                     const competitors = event.competitions[0].competitors;
                     const totalScore = competitors.reduce((sum, team) => sum + parseInt(team.score), 0);
-                    console.log(`Game ID: ${event.id}, Over/Under: ${overUnder}, Total Score: ${totalScore}`);
+                    const teamNames = competitors.map(team => team.team.displayName).join(' vs ');
+
+                    console.log(`Game: ${teamNames}, Over/Under: ${overUnder}, Total Score: ${totalScore}`);
                 }
             });
         }
